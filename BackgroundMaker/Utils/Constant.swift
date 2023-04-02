5b2149440f96b64b8292a08bdabab6d63f38940e
Heonjin Ha <weizen511@gmail.com>
2023-04-02 23:53:15 +0900

    Widget Maker 분리 및 디렉토리 정리

---
+++ BackgroundMaker/Utils/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  BackgroundMaker
+//
+//  Created by HeonJin Ha on 2022/09/30.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    
+    //MARK: - 공통 변수
+    static let deviceSize = UIScreen.main.bounds.size
+    
+    //MARK: - Widget Constants
+
+    static let reuseID_WidgetIconCell = "WidgetIconCell"
+    static let coreDataEntityName = "BackgroundMaker"
+    static let appGroupID = "group.ZH5GA3W8UP.com.weizen.BackgroundMaker"
+
+}
